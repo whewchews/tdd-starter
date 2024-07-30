adb3af95aff5e3c44b7e867f7ef52e1c07a1a38a
whewchews <whewchews@gmail.com>
2024-07-31 00:41:39 +0900

    feat: initial commit with next

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>
+          Get started by editing&nbsp;
+          <code>app/page.tsx</code>
+        </p>
+      </div>
+    </main>
+  );
+}
