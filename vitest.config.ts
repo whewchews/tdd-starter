e80c5e4814265bf462fc7cd7f17310a54c519e4c
whewchews <whewchews@gmail.com>
2024-07-31 01:00:15 +0900

    chore: vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-// vitest.config.ts
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 /// <reference types="vitest" />
@@ -8,12 +7,35 @@ export default defineConfig({
   root: "./",
   test: {
     setupFiles: "./setupTests.ts",
-    include: ["src/**/*.test.tsx", "src/**/*.test.ts"],
+    include: ["app/**/*.test.tsx", "app/**/*.test.ts"],
     css: true,
     pool: "vmThreads",
     poolOptions: {
       useAtomics: true,
     },
     testTimeout: 3000,
+    onConsoleLog(log, type) {
+      if (log.includes("inside a test was not wrapped in act")) return false;
+      if (
+        log.includes(
+          "Warning: ReactDOM.render is no longer supported in React 18"
+        )
+      )
+        return false;
+      if (log.includes("should be wrapped into act")) return false;
+    },
+    // coverage: {
+    // 	provider: 'istanbul',
+    // 	include: [
+    // 		'app/**/*.{ts,tsx}'
+    // 	],
+    // 	exclude: [
+    // 		'node_modules/**',
+    // 		'dist/**',
+    // 		'src/**/*.{test,spec}.{ts,tsx}',
+    // 	],
+    // 	all: true,
+    // 	reporter: ['json', 'text', 'html'],
+    // },
   },
 });
