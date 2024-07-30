dbf831fad88888051b2c994ec0db82344b676927
whewchews <whewchews@gmail.com>
2024-07-31 01:06:19 +0900

    chore: setup testing library

--- app/cosmos/hello.test.ts
+++ app/cosmos/hello.test.ts
@@ -1,14 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import helloStory from "./Hello.fixture";
 import { expect, test } from "vitest";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
 });
 
-import { renderToString } from "react-dom/server";
-import helloStory from "./Hello.fixture";
-import { expect, test } from "vitest";
-
 test("render hello", () => {
-  document.body.innerHTML = renderToString(helloStory);
-  expect(document.body.innerHTML).toBe("<h1>Hello World!</h1>");
+  render(helloStory);
+  expect(
+    screen.getByRole("heading", { name: "Hello World!" })
+  ).toBeInTheDocument();
 });
